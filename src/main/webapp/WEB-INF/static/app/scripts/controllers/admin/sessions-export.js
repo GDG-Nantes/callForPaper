e0fec74fdf09a54db1bf78efe49a421332d55319
Benjamin Petetot <bpetetot@gmail.com>
2016-08-12 10:34:51 +0200

    Change JSON structure of exported sessions

--- src/main/webapp/WEB-INF/static/app/scripts/controllers/admin/sessions-export.js
+++ src/main/webapp/WEB-INF/static/app/scripts/controllers/admin/sessions-export.js
@@ -21,45 +21,7 @@ angular.module('CallForPaper')
 		 */
 		AdminSession.query().$promise.then(function(sessionsTmp) {
 			$scope.sessions = _.map(sessionsTmp, function(session) {
-				return {
-					id: session.email,
-					topspeaker: false,
-					firstname: session.firstname,
-		            lastname: _.capitalize(session.name.toLowerCase()),
-		            image: "todo à la main ?",
-		            category: {
-		                class: session.track,
-		                title: _.capitalize(session.track)
-		            },
-		            ribon: {
-		                class: "gde",
-		                title: "GDE",
-		                link: "https://developers.google.com/experts/",
-		                tile_long: "Google Developer Expert"
-		            },
-		            company: session.company,
-		            about: session.bio,
-		            socials: [
-		                {class: "google-plus", link: session.googlePlus},
-		                {class: "twitter", link: session.twitter},
-		                {class: "github", link: session.github},
-		                {class: "site", link: session.social}
-		            ],
-		            session: [{
-		            	"id":"s4",
-					    "title":session.sessionName,
-					    "confRoom":"Salle Titan",
-					    "desc":session.description,
-					    "type":session.track,
-					    "difficulty":parseInt(session.difficulty+"0"+session.difficulty),
-					    "all" : false,
-					    "lang":"fr",
-					    "hour":"",
-					    "video":"",
-					    "slides":"",
-					    "speakers":[session.email]
-		            }]
-				};
+				return session.toJSON();
 			});
 		});
 }]);
